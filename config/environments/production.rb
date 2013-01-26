5ebcd65218c7a19b74032bb6d57f8979d4e735ab
Sky Rumsey <sky@arttracker.co.uk>
2013-01-26 12:15:24 +0000

    removed produciton log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,6 @@ Filmlovers::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  Rails.logger = Logger.new(STDOUT)
-  
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5

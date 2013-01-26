fdfb3f13708de230be30800104369f87e87e314c
Sky Rumsey <sky@arttracker.co.uk>
2013-01-26 00:35:37 +0000

    mongoid

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,12 +1,2 @@
 include Mongo
-def get_connection
-  return @db_connection if @db_connection
-  return MongoClient.new if Rails.env != 'production'
-  db = URI.parse(ENV['MONGOHQ_URL'])
-  db_name = db.path.gsub(/^\//, '')
-  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-  @db_connection
-end
-
-$mongo = get_connection
+$mongo = Mongo::Connection.new

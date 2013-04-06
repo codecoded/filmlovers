f5f9d8c1d104e591fe670e211688ea5791c96d8d
Sky Rumsey <sky@arttracker.co.uk>
2013-04-06 23:18:25 +0100

    user films bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def show
     results_page =  UserService.new(user).paged_list(user_action, order, by, params[:page].to_i, 70)
-    @films_page = FilmsPagePresenter.new user, results_page, user_action
+    @films_page = FilmsPagePresenter.new current_user, results_page, user_action
   end
 
   def user

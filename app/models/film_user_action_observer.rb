fd6efe423a0ec1bc3dcbdf0686eb7737e30902c6
Sky Rumsey <sky@arttracker.co.uk>
2013-05-29 20:17:14 +0100

    films recommendations more strict

--- app/models/film_user_action_observer.rb
+++ app/models/film_user_action_observer.rb
@@ -5,7 +5,8 @@ class FilmUserActionObserver < Mongoid::Observer
     Log.debug "Created film_user_action: #{film_user_action}"
     comparer = ComparisonService.new film_user_action.user
     comparer.friends_sorted_by_rating(20).each do |rated_friend|
-      film_user_action.user.recommendations.create! friend: rated_friend[:friend], recommendable: film_user_action.film 
+      # return if Recommendation.recommended? film_user_action.user, rated_friend[:friend], film_user_action.film 
+      film_user_action.user.recommendations.create friend: rated_friend[:friend], recommendable: film_user_action.film, auto: true
     end
   end
 
--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,15 +2,29 @@ class Recommendation
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  before_validation :new_recommendation?
   after_create :notify
 
   belongs_to :user
   belongs_to :friend, :class_name => "User"
-
   belongs_to :recommendable, polymorphic: true
 
   validates_presence_of :friend
+  validates_presence_of :user
+  # validates :recommendable, uniqueness: {message: "this film has already been recommended", scope: [:friend, :user]}, presence: true
 
+  index({ user: 1, friend: 1, recommendable: 1}, { unique: true, name: "recommendation_index", background: true })
+
+  field :auto, type: Boolean, default: true
+
+  def self.recommended?(user, friend, recommendable)
+    where(user: user, friend: friend, recommendable: recommendable).exists?
+  end
+
+  private
+  def new_recommendation?
+    !(Recommendation.recommended?(user, friend, recommendable) or friend.film_actioned?(recommendable, :watched))
+  end
 
   def notify
   end

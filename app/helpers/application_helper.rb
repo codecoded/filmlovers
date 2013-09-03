a870aa8f70ae66bae48d795870c75228da04817b
soultripper <sky@filmlovers.co.uk>
2013-09-03 13:24:07 +0100

    added logging for staging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def present(object, klass = nil)
-    klass ||= "{object.class}Presenter".constantize
+    klass ||= "#{object.class}Presenter".constantize
     presenter = klass.new(object, self)
     yield presenter if block_given?
     presenter

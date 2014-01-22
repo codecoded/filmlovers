78c28a485e48e2f19fe50bb275f11fa6663f0403
soultripper <sky@filmlovers.co.uk>
2014-01-22 17:50:47 +0000

    change to fb presenter

--- app/presenters/facebook_presenter.rb
+++ app/presenters/facebook_presenter.rb
@@ -1,15 +1,15 @@
 class FacebookPresenter
 
   def self.recommendation_message(recommendation)
-    receiver = recommendation.user
-    sender = recommendation.friend
+    sender = recommendation.user
+    receiver = recommendation.friend
 
     if !sender.channels[:facebook]
       return "#{receiver.username} recommends #{recommendation.film.title}. #{recommendation.comment}" 
     end
 
     if receiver.facebook_events.recent.count < 2
-      "@[#{sender.passport_for(:facebook).uid}] recommends #{recommendation.film.title}. #{recommendation.comment}"
+      "@[#{receiver.passport_for(:facebook).uid}] recommends #{recommendation.film.title}. #{recommendation.comment}"
     else
       "You have several new film recommendations! Click here to discover new films to watch."
     end

69247efde9db5c81239b82e791d7cea453482945
soultripper <sky@filmlovers.co.uk>
2014-05-02 01:30:11 +0700

    default to US for storefront

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -3,7 +3,7 @@ Rails.application.config.to_prepare do
     config.ios_app = 'filmlovr_app'
     config.page_size = 21
     config.itunes_affiliate = '10lne2'
-    config.storefront_id = 143444 #USA: 143441, UK: 143444
+    config.storefront_id = 143441 #USA: 143441, UK: 143444
   end
 
   Geocoder.configure(:cache => $redis)
